af965edafdc8b0559702133ed75074fe1bb40d17
nsardinia <52211680+nsardinia@users.noreply.github.com>
2022-11-23 15:29:02 -0500

    next commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/graph-visualization-frontend/',
-    plugins: [vue()]
+    //plugins: [vue()]
 })
